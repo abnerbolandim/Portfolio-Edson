4ca9a6f5500d2a8a0393e11f4a07c94c4c95e5ad
abnerbolandim <contato.abnerbolandim@gmail.com>
2024-04-06 17:36:28 +0100

    Add scroll to top / js and css

---
+++ js/scroll.js
@@ -0,0 +1,18 @@
+function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }
+  
+  function handleScroll() {
+    var scrollToTopBtn = document.querySelector(".scroll-to-top");
+    if (window.scrollY > 300) {
+      scrollToTopBtn.classList.add("showBtn");
+    } else {
+      scrollToTopBtn.classList.remove("showBtn");
+    }
+  }
+  
+  window.addEventListener("scroll", handleScroll);
+  
